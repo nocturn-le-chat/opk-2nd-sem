7ce7f372ca1bef350418b4a74839277b0b4b0dea
nocturn-le-chat <zimlin.nazar@yandex.ru>
2026-05-13 23:16:18 +0700

    Initial commit

--- linked_lists/main.py
+++ linked_lists/main.py
@@ -1,1 +1,10 @@
- 
+class Linked_list:
+    pass
+
+    # Конструктор
+    def from_list(list):
+        pass
+
+    # Деконструктор
+    def to_list(linked_list):
+        pass
--- linked_lists/unit_test.py
+++ linked_lists/unit_test.py
@@ -1,2 +1,1 @@
-from main import linked_list
-from random import randint
+from main import Linked_list as subj
