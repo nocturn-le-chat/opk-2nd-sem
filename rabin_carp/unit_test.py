b30efba42367cec0d3682edbdd1364474225df02
nocturn-le-chat <zimlin.nazar@yandex.ru>
2026-05-13 16:14:31 +0700

    Tweaked unit tests messages

--- rabin_carp/unit_test.py
+++ rabin_carp/unit_test.py
@@ -8,7 +8,7 @@ assert rabin_carp(1, "one") == None, "ERROR: non-string argument chewing"
 assert rabin_carp(False, 0) == None, "ERROR: non-string argument chewing"
 assert rabin_carp(0, ["zero", "hero"]) == None, "ERROR: non-string argument chewing"
 assert rabin_carp("1234567890", "123") == None, "ERROR: chewing pattern bigger than origin string"
-print("\nStatic tests done...")
+print("Static tests done...\n")
 
 sample_text = "Психосфера была на острие общественного и делового интереса, как космос и космонавтика в эпоху первых полетов середины-конца XX-го века. " \
               "Однако, не все шло гладко. Скоро стало понятно, что открытые пространства - это не райские кущи. " \
@@ -19,7 +19,7 @@ try:
     cursor = randint(0, len(sample_text)-pattern_len)
     pattern = sample_text[cursor:cursor+pattern_len+1]
     if findall(pattern, sample_text) != rabin_carp(pattern, sample_text): raise ValueError
-    else: print("\nDynamic tests done...")
+    else: print("Dynamic tests done...\n")
 except ValueError: print("ERROR: bad searching algorithm")
 
-print("\n\n...Done!")
+print("\t\t...Done!")
