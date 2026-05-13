43cfb7bdf8db4d32a9359cd16b9fc02b5565c2a2
nocturn-le-chat <zimlin.nazar@yandex.ru>
2026-05-13 15:32:10 +0700

    Added comments

--- rabin_carp/main.py
+++ rabin_carp/main.py
@@ -1,18 +1,21 @@
 def rabin_carp_operator(pattern, string):
+    # Хеширование
     p_hash = sum([ord(pattern[k])*2048**k for k in range(len(pattern))])%2099
     p_len, s_len = len(pattern), len(string)
-
     matchbox = list()
+
+    # Первичная проверка
     for cursor in range(0, s_len-p_len+1):
         slice = string[cursor : cursor+p_len]
         sl_hash = sum([ord(slice[k])*2048**k for k in range(len(slice))])%2099
         if p_hash == sl_hash:
             matchbox.append(slice)
+
+    # Вторичная побуквенная проверка
     p_letter_hash = [ord(k) for k in pattern]
     for n in matchbox:
         n_letter_hash = [ord(k) for k in n]
         if p_letter_hash != n_letter_hash:
             matchbox.remove(n)
+    
     return matchbox
-
-print(rabin_carp_operator("leet", "leetspeakleet"))
